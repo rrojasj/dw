74ec8c8ac2d0b8465a45b4adf131aefc10eba7a5
Roberto Rojas <robjimn@amazon.com>
2024-08-18 11:26:24 -0600

    50% del sitio Web.

--- Proyecto/js/general.js
+++ Proyecto/js/general.js
@@ -5,3 +5,15 @@ document.querySelector('.navbar-style').addEventListener('mouseenter', function(
 document.querySelector('.navbar-style').addEventListener('mouseleave', function() {
     document.querySelector('.navbar-logo img').src = 'imgs/dw_p1_logo_lb_sm_transp.svg';
 });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//         anchor.addEventListener('click', function (e) {
+//             e.preventDefault();
+            
+//             document.querySelector(this.getAttribute('href')).scrollIntoView({
+//                 behavior: 'smooth'
+//             });
+//         });
+//     });
+// });
